9b86161814e516ce21fd57c995d94633963049a9
swest19@lsu.edu <swest19@lsu.edu>
2025-03-06 13:39:42 -0600

    restructuring dataframe and adding some notes

--- Marissa-Cue-Selection.R
+++ Marissa-Cue-Selection.R
@@ -1,13 +1,15 @@
-load("psychling/responses_metadata_2025-02-27.Rdata")
 library(dplyr)
-d <- combined_meta %>%
-  select(cue,condition,response, AoA_Kup_lem, Lg10WF, Lg10CD) %>%
-  unique() %>%
-  mutate(AoA_inv = max(AoA_Kup_lem, na.rm =T) - AoA_Kup_lem ) %>%
-  group_by(cue,response,condition) %>%
-  mutate(comp = sum(AoA_inv,Lg10CD,Lg10WF, na.rm = T)) %>%
-  filter(condition == "child") %>%
-  ungroup() %>%
-  select(cue, comp) %>%
-  unique()
-  
+load("psychling/responses_formatted_1032023.Rdata")
+
+full_df_rev_coded <- full_df %>%
+  mutate(condition = factor(COND_ID, levels = c(1,2,3), labels = c("peer", "child-o", "short")),.after = response) %>%
+  rename(word_length = wl) %>%
+  select(-COND_ID) %>%
+  mutate(aoa_inv = max(aoa, na.rm = T) - aoa,
+         word_length_inv = max(word_length) - word_length, .after=syll)
+
+## Objectives are to: 
+# 1. Compute an average score by cue for reverse-coded word length, reverse-coded aoa, word frequency, and contextual diversity. 
+##   AoA and word length have already been reverse coded. 
+# 2. Compute a composite "child-orientedness" score for each cue by summing across the averaged psycholinguistic variables.
+
