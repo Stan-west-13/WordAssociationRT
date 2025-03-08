a4e3fb03689c8bd52db974ffdc603085b333060b
Swest13 <swest19@lsu.edu>
2025-03-08 13:23:42 -0600

    Starting to merge mappings between studies

--- 003-CreateSQL-Database.R
+++ 003-CreateSQL-Database.R
@@ -10,6 +10,30 @@ library(tidyr)
 ## Load data
 load("psychling/responses_metadata_2025-03-07.Rdata")
 source("R/dbStatementHelpers.R")
+source("R/dbGetHelpers.R")
+
+## Create studies table
+studies <- data.frame(id = c(1,2,3), 
+                      label = c("Adult_Association1", "Adult_Association2", "WordAssociationRT"), 
+                      description = c("Movers and stayers in RSA were assessed 
+                                       by collecting word associations from 60 
+                                       cues with high and low probability of 
+                                       having strong context effects. Collection 1",
+                                      "Movers and stayers in RSA were assessed 
+                                       by collecting word associations from 60 
+                                       cues with high and low probability of 
+                                       having strong context effects. Collection 2",
+                                      "Word associations were collected for 64 cues
+                                       that elicit child-oriened and 
+                                       non-child-oriented resoponses in SWOW.
+                                       Response times were collected to see how changing
+                                       task instructions (context or no context) effects
+                                       associative behavior"))
+
+
+
+
+
 ## Create psychling tables
 kuperman_table <- read_csv("psychling/AoA_51715_words.csv") %>%
   mutate(
@@ -32,7 +56,7 @@ metadata <- combined_meta %>%
   select(PPID = participant, condition, cue, strength_strat, type, response, rt = key_resp_cue.rt, 25:49) %>%
   mutate(rt_mili = rt * 1000, .after = rt) %>%
   group_by(PPID) %>%
-  mutate(cue_order = seq(1,n(),1), .after = cue) %>%
+  mutate(cue_order = seq_len(n()), .after = cue) %>%
   ungroup()
 
 
@@ -64,13 +88,49 @@ responses_table <- resp_behavior_table %>%
   select(response) %>%
   distinct() %>%
   arrange(response) %>%
-  mutate(id = seq_len(n()))
+  mutate(id = seq_len(n()),
+         study_id = 3) %>%
+  select(id, study_id,response)
 
 ## Response behavior table setup finish
 response_behavior_table <- resp_behavior_table %>%
   left_join(responses_table %>% rename(response_id = id), by = "response") %>%
   select(id, subject_id , cue_order, cue_id, response_id, -response) %>%
   filter(!is.na(response_id))
+
+## Load existing response maps and ids and add study ID
+conn_adult_collection1 <- dbConnect(RSQLite::SQLite(),"databases/semantic_association_validation-crc.db")
+adult_collection1_rm <- dbGetQuery(conn_adult_collection1, "SELECT * FROM response_map")
+adult_collection1_respid <- dbGetQuery(conn_adult_collection1, "SELECT * FROM responses") %>%
+  mutate(study_id = 1,.after = id)
+dbDisconnect(conn_adult_collection1)
+
+conn_adult_collection2 <- dbConnect(RSQLite::SQLite(),"databases/semantic_association_validation-crc_2.db")
+adult_collection2_rm <- dbGetQuery(conn_adult_collection2, "SELECT * FROM response_map")
+adult_collection2_respid <- dbGetQuery(conn_adult_collection2, "SELECT * FROM responses") %>%
+  mutate(study_id = 2, .after = id)
+dbDisconnect(conn_adult_collection2)
+
+
+
+
+## Join responses from second adult associaton collection into the first and
+## give new responses new unique ids.
+studies_response <- rbind(adult_collection1_respid, adult_collection2_respid) %>%
+  mutate(response_id = id,
+         id = seq.int(n())) %>%
+  select(id, study_id, response_id)
+
+
+
+
+
+
+
+
+
+
+
 
 
 ## Cues and resposnes table
