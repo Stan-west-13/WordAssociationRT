be29fcda5b3de15d7eaa14bd8fcffe51b95658f3
mgarce3 <mgarce3@lsu.edu>
2025-08-19 17:07:20 -0500

    Changed up strategy based on Stan's suggestions. Added cues and made it so that the conditions are in the df "cue_combo".

--- 010-Excel_Sheet_Generate/010-Excel_Sheet_Generate.R
+++ 010-Excel_Sheet_Generate/010-Excel_Sheet_Generate.R
@@ -8,29 +8,34 @@ library(readxl)
 # Read in list of valid combinations of 3 squares presented out of 8
 og_combos_df <- read_xlsx("010-Excel_Sheet_Generate/for_generating_sq_combo.xlsx")
 
+# Read in csv of cue words and extract one cues for now
+stim_og <- read.csv("010-Excel_Sheet_Generate/stim_64_NNVB.csv")
+stim_64 <- data.frame(cue = stim_og$cue)
+
 # Create data frame of no load combos
 no_load_combos <- setNames(
   data.frame(matrix(data = 1, nrow = 32, ncol = 8)),
   paste0("sq", 1:8)) %>% 
-  mutate(load_condition = "no_load", .before = "sq1") %>% 
-  mutate(test_condition = "match")
+  mutate(condition = "no_load", .before = "sq1")
+
+# Create data frame of cues, conditions, and probability
+x <-rep(1:nrow(stim_64), 3)
+cue_combos <- data.frame(cue = stim_64[x,]) %>% 
+  arrange(cue) %>% 
+  mutate(condition = rep(c("load", "load", "no_load"), nrow(.)/3)) %>% 
+  mutate(trial_type = rep(c("match", "mismatch", "match"), nrow(.)/3)) %>% 
+  mutate(prob = 1)
 
 ##################################################################################################
 
 # Will be turned into a loop: 
 
 ## Add load condition column and randomly choose 32 displays
-load_combos <- og_combos_df %>% 
-  mutate(load_condition = "load", .before = "sq1") %>% 
-  sample_n(., 32)
-
-## Randomly give each combo either "match" or "mismatch" test_condition (16 each)
-load_combos$test_condition <- sample(rep(c("match", "mismatch"), nrow(load_combos)/2))
+load_combos <- sample_n(og_combos_df, 32) %>% 
+  mutate(condition = "load", .before = "sq1")
 
 ## New data frame with both load and no load, randomly shuffled
 all_combos <- rbind(load_combos, no_load_combos)
-
-
 
 
 
