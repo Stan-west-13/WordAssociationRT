39aaee9dc3afd9d43f49432ded69f8a88eebeb0d
Swest13 <swest19@lsu.edu>
2025-08-25 17:50:34 -0500

    Added annotations

--- 010-Excel_Sheet_Generate_SHW.R
+++ 010-Excel_Sheet_Generate_SHW.R
@@ -23,31 +23,46 @@ cue_combos <- expand.grid(cue = stim$cue, condition = c("load","no_load"),
 trial_type_probs <- data.frame(TT = c("match", "mismatch"), prob = c(0.5,0.5))
 condition_probs <- data.frame(condition = c("load","no_load"), prob = c(0.5,0.5))
 
-map(seq.int(1,20,1), function(x){
-  df <- data.frame(pp = rep(x, 64))
-  df$cue <- sample(stim$cue, 64, replace = FALSE)
-  df$condition <- 0
-  while(any(rle(df$condition)$lengths > 3)){
+## Map is like a loop, this is itterating over 20 integers (participants) and running the
+## code defined in the anonymous function:
+x <- map(seq.int(1,20,1), function(x){
+  df <- data.frame(pp = rep(x, 64)) ## initailize dataframe with the participant id
+  df$cue <- sample(stim$cue, 64, replace = FALSE) ## randomly sample the 64 cues without replacement
+  df$condition <- 0 ## set a placeholder for condition to use with the while loop
+  while(any(rle(df$condition)$lengths > 3)){ ## This is saying while there are any runs > 3 in the condition column for a participant, keep running the for loop.
     for (i in 1:nrow(df)){
-      df$condition[i] <- sample(condition_probs$condition,size = 1, prob = condition_probs$prob)
+      df$condition[i] <- sample(condition_probs$condition,size = 1, prob = condition_probs$prob) # sample a condition one at a time with probabilities sampled from the dataframes on lines 23 & 24
         if (df$condition[i] == "load"){
-          condition_probs$prob[condition_probs$condition == "load"] <- condition_probs$prob[condition_probs$condition == "load"] - (1/64)
-          df$TT[i]  <- sample(trial_type_probs$TT, size = 1, prob = trial_type_probs$prob)
-            if (df$TT[i] == "match"){
-              trial_type_probs$prob[trial_type_probs$TT == "match"] <- trial_type_probs$prob[trial_type_probs$TT == "match"] - (1/32)
+          condition_probs$prob[condition_probs$condition == "load"] <- condition_probs$prob[condition_probs$condition == "load"] - (1/64) # if the current iteration is load, subtract 1/64
+          df$TT[i]  <- sample(trial_type_probs$TT, size = 1, prob = trial_type_probs$prob) ## also sample a trial type for load
+            if (df$TT[i] == "match"){ ## if that trial type is match,
+              trial_type_probs$prob[trial_type_probs$TT == "match"] <- trial_type_probs$prob[trial_type_probs$TT == "match"] - (1/32) # subtract 1/32 from the match probability 
             }else{
-              trial_type_probs$prob[trial_type_probs$TT == "mismatch"] <- trial_type_probs$prob[trial_type_probs$TT == "mismatch"] - (1/32)
+              trial_type_probs$prob[trial_type_probs$TT == "mismatch"] <- trial_type_probs$prob[trial_type_probs$TT == "mismatch"] - (1/32) # otherwise subtract it from the mismatch prob
             }
         } else{
-          condition_probs$prob[condition_probs$condition == "no_load"] <- condition_probs$prob[condition_probs$condition == "no_load"] - (1/64)
-          df$TT[i] <- "match"
+          condition_probs$prob[condition_probs$condition == "no_load"] <- condition_probs$prob[condition_probs$condition == "no_load"] - (1/64) ## where condition is no_load, subtract 1/64
+          df$TT[i] <- "match" ## and assign it "match" because no_load only gets match
         }
       
     }
-  condition_probs$prob <- 0.5
+  condition_probs$prob <- 0.5 ## Finally reset the probabilities in case the for loop has to run multiple times for a participant
   trial_type_probs$prob <- 0.5
   }
   
   return(df)
 })
+
+
+## Check that all lists have expected condition and trial type distributions
+
+map(x,function(x){
+  x |>
+    group_by(condition, TT) |>
+    summarize(n = length(cue))
+})
+
+
+
+
 
