bcf25cc13b541fc105762598f6d9dd97a4cb8632
Stanley H West <swest19@lsu.edu>
2025-09-10 10:26:49 -0500

    reminder to take out trial type repeats

--- 010-Excel_Sheet_Generate_SHW.R
+++ 010-Excel_Sheet_Generate_SHW.R
@@ -22,7 +22,9 @@ cue_combos <- expand.grid(cue = stim$cue, condition = c("load","no_load"),
   arrange(cue)
 
 
-trial_type_probs <- data.frame(TT = c("match", "mismatch"), prob = c(0.5,0.5))
+trial_type_probs_L <- data.frame(TT = c("match", "mismatch"), prob = c(0.5,0.5))
+trial_type_probs_NL <- data.frame(TT = c("match", "mismatch"), prob = c(0.5,0.5))
+
 condition_probs <- data.frame(condition = c("load","no_load"), prob = c(0.5,0.5))
 
 ## Map is like a loop, this is itterating over 20 integers (participants) and running the
@@ -31,13 +33,13 @@ x <- map(c(1:2), function(x){
   combos <- og_combos_df
   df <- data.frame(pp = rep(x, each = 64), sq1 = NA,sq2=NA,sq3=NA,sq4=NA,sq5=NA,sq6=NA,sq7=NA,sq8=NA) ## initailize dataframe with the participant id
   df$cue <- sample(stim$cue, 64, replace = FALSE) ## randomly sample the 64 cues without replacement
-  df$condition <- 0 ## set a placeholder for condition to use with the while loop
-  while(any(rle(df$condition)$lengths > 3)){ ## This is saying while there are any runs > 3 in the condition column for a participant, keep running the for loop.
+  df$TT <- 0 ## set a placeholder for condition to use with the while loop
+  while(any(rle(df$TT)$lengths > 3)){ ## This is saying while there are any runs > 3 in the condition column for a participant, keep running the for loop.
     for (i in 1:nrow(df)){
       df$condition[i] <- sample(condition_probs$condition,size = 1, prob = condition_probs$prob) # sample a condition one at a time with probabilities sampled from the dataframes on lines 23 & 24
         if (df$condition[i] == "load"){
           condition_probs$prob[condition_probs$condition == "load"] <- condition_probs$prob[condition_probs$condition == "load"] - (1/64) # if the current iteration is load, subtract 1/64
-          df$TT[i]  <- sample(trial_type_probs$TT, size = 1, prob = trial_type_probs$prob) ## also sample a trial type for load
+          df$TT[i]  <- sample(trial_type_probs_L$TT, size = 1, prob = trial_type_probs_L$prob) ## also sample a trial type for load
           df[i,2:9] <- slice_sample(combos[!combos %in% df[,2:9]], n = 1,replace = FALSE)
             if (df$TT[i] == "match"){ ## if that trial type is match,
               trial_type_probs$prob[trial_type_probs$TT == "match"] <- trial_type_probs$prob[trial_type_probs$TT == "match"] - (1/32) # subtract 1/32 from the match probability 
@@ -46,17 +48,25 @@ x <- map(c(1:2), function(x){
             }
         } else{
           condition_probs$prob[condition_probs$condition == "no_load"] <- condition_probs$prob[condition_probs$condition == "no_load"] - (1/64) ## where condition is no_load, subtract 1/64
-          df$TT[i] <- "match" ## and assign it "match" because no_load only gets match
+          df$TT[i] <- sample(trial_type_probs_NL$TT, size = 1, prob = trial_type_probs_NL$prob) ## and assign it "match" because no_load only gets match
           df[i,2:9] <- rep(1,8)
+          if (df$TT[i] == "match"){ ## if that trial type is match,
+            trial_type_probs_NL$prob[trial_type_probs_NL$TT == "match"] <- trial_type_probs_NL$prob[trial_type_probs_NL$TT == "match"] - (1/32) # subtract 1/32 from the match probability 
+          }else{
+            trial_type_probs_NL$prob[trial_type_probs_NL$TT == "mismatch"] <- trial_type_probs_NL$prob[trial_type_probs_NL$TT == "mismatch"] - (1/32) # otherwise subtract it from the mismatch prob
+          }
         }
       
     }
    
   condition_probs$prob <- 0.5 ## Finally reset the probabilities in case the for loop has to run multiple times for a participant
-  trial_type_probs$prob <- 0.5
+  trial_type_probs_NL$prob <- 0.5
+  trial_type_probs_L$prob <- 0.5
   }
   
   df_split <- split(df, df$condition)
+  
+  #df_split$load <-  Put functionality here to not repeat matches and mismatch more than 3 times.
   
   write.csv(df_split$load, paste0("test_psychopy/TTA_",sprintf("%03d",unique(df$pp)),"_trial_list_L.csv" ),row.names = FALSE)
   write.csv(df_split$no_load, paste0("test_psychopy/TTA_",sprintf("%03d",unique(df$pp)),"_trial_list_NL.csv" ),row.names = FALSE)## Add N and NL to end of trial list for load and no load.
