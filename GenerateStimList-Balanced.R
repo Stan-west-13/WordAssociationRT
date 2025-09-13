28bd4dea377c60da148df6aa368ef26fbe4d5dde
Swest13 <swest19@lsu.edu>
2025-09-13 15:11:01 -0500

    Create new folder for each participant

--- GenerateStimList-Balanced.R
+++ GenerateStimList-Balanced.R
@@ -64,8 +64,8 @@ x <- map(c(1), function(x){ ## change values in c() for more participants
     trial_type_probs_L$prob <- 0.5
   }
   
-  
-  write.csv(df_split$load, paste0("test_psychopy/TTA_",sprintf("%03d",unique(df_split$load$pp)),"_trial_list_L.csv" ),row.names = FALSE)
-  write.csv(df_split$no_load, paste0("test_psychopy/TTA_",sprintf("%03d",unique(df_split$no_load$pp)),"_trial_list_NL.csv" ),row.names = FALSE)## Add N and NL to end of trial list for load and no load.
+  dir.create(paste0("stim_files/TTA_",sprintf("%03d",unique(df_split$no_load$pp))))
+  write.csv(df_split$load, paste0("stim_files/TTA_",sprintf("%03d",unique(df_split$load$pp)),"/TTA_",sprintf("%03d",unique(df_split$load$pp)),"_trial_list_L.csv" ),row.names = FALSE)
+  write.csv(df_split$no_load, paste0("stim_files/TTA_",sprintf("%03d",unique(df_split$no_load$pp)),"/TTA_",sprintf("%03d",unique(df_split$no_load$pp)),"_trial_list_NL.csv" ),row.names = FALSE)## Add N and NL to end of trial list for load and no load.
   return(df_split)
 })
