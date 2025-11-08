815ff0402231c4ad5d76acfb779d5617c8937e1b
mgarce3 <mgarce3@lsu.edu>
2025-11-08 16:32:51 -0600

    finished .docx table draft

--- 011-Demographics.R
+++ 011-Demographics.R
@@ -6,14 +6,14 @@ library(pastecs)
 library(psych)
 library(tidyverse)
 library(rstatix)
+library(flextable)
 
 temp_df <- read_xlsx('Demographics_TTA.xlsx')
 
-################################################
-# Making new data frame to factorize columns
-################################################
+#########################
+# Making new data frame
+#########################
 
-## n participants per group:
 demo_df <- temp_df %>%
   mutate_at(c('subject_id', 'condition', 'education', 'gender', 'income', 'race', 'ethnicity', 
               'parent', 'toddler_interaction', 'toddler_int_frequency'), as.factor)
@@ -22,19 +22,47 @@ demo_df <- temp_df %>%
 demo_summary <- demo_df %>% 
   group_by(condition) %>% 
   summarize(n = n(),
+            n_male = length(which(gender == 'male')),
             mean_age = mean(age, na.rm = T),
             sd_age = sd(age, na.rm = T),
             min_age = min(age, na.rm = T),
             max_age = max(age, na.rm = T),
-            NA_values = sum(if_any(everything(), is.na)))
+            NA_age = sum(if_any(age, is.na)),
+            weekly_int = sum(toddler_int_frequency %in% c('daily', 'weekly', 'few/week'))/ n(),
+            less_int = sum(toddler_int_frequency %in% c('monthly', 'rare', 'never'))/ n()
+            ) 
+demo_summary$mean_age <- round(demo_summary$mean_age, 2)
+demo_summary$sd_age <- round(demo_summary$sd_age, 2)
+demo_summary$weekly_int <- round(demo_summary$weekly_int, 2)
+demo_summary$less_int <- round(demo_summary$less_int, 2)
 
-x <- demo_df %>% 
-  group_by(condition, education) %>% 
-  summarize(n_edu = n())
+##########################
+# Making table for poster
+##########################
 
-y <- demo_df %>% 
-  group_by(condition) %>% 
-  summarise(n_associates = sum(education == 'associates')/ n(),
-            n_highschool = sum(education == 'high school graduate')/ n(),
-            )
+demo_table <- flextable(data = head(demo_summary),
+                        col_keys = c('condition','col1', 'n', 'col2',
+                                     'mean_age', 'sd_age', 'min_age', 'max_age', 'NA_age', 'col3',
+                                     'weekly_int', 'less_int')) |> 
+                          width(j = 'col1', width = .2) |>
+                          empty_blanks()
+
+demo_table <- demo_table %>% 
+  add_header_row(., values = c('','', 'Age', 'Toddler Interaction Frequency'), 
+                 colwidths = c(2, 2, 6, 2)) %>% 
+  align(align = 'center', part = 'all') %>% 
+  set_header_labels(., n = 'n(male)', mean_age = 'mean', sd_age = 'SD', min_age = 'min',
+                    max_age = 'max', NA_age = 'NA values', weekly_int = 'Weekly +', 
+                    less_int = 'Less than weekily') %>% 
+  bg(bg = '#FCFBFF', part = 'all')
+  
+save_as_docx(demo_table, path = "demographics_table.docx")
+
+
+
+
+
+
+
+
 
