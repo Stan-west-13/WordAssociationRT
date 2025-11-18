c3217ec974ba5d5b3e20a150c7c8f87600ec57de
Christopher Cox <chriscox@lsu.edu>
2025-11-18 17:31:07 -0600

    need to multiply qt() * se

--- 006-Creative_Stereotypy-crc.R
+++ 006-Creative_Stereotypy-crc.R
@@ -6,6 +6,44 @@ library(ggplot2)
 creativity <- readRDS("tmp-data/creativity-all.rds")
 stereotypy <- readRDS("tmp-data/stereotypy-all.rds")
 
+x <- creativity |>
+  count(condition, cue, corrected_response) |>
+  group_by(condition, cue) |>
+  summarize(score = mean(n == 1)) |>
+  group_by() |>
+  pivot_wider(id_cols = cue, names_from = condition, values_from = score)
+
+x_contr <- x |>
+  mutate(
+    child_peer = child - peer,
+    child_short = child - short,
+    child_creative = child - creative,
+    peer_short = peer - short,
+    peer_creative = peer - creative,
+    short_creative = short - creative
+  ) |>
+  select(-child, -peer, -short, -creative) |>
+  pivot_longer(
+    cols = c(child_peer:short_creative),
+    names_to = "contrast",
+    values_to = "scorediff"
+  )
+
+x_contr_avg <- x_contr |>
+  group_by(contrast) |>
+  summarize(
+    mean_diff = mean(scorediff),
+    s = sd(scorediff),
+    se = s / sqrt(n()),
+    ci = qt(.025, 60 - 4, lower.tail = FALSE) * se
+  ) |>
+  ungroup()
+
+x_contr_avg |>
+  ggplot(aes(x = contrast, y = mean_diff)) +
+    geom_pointrange(aes(ymin = mean_diff - ci, ymax = mean_diff + ci))
+
+               
 
 d <- list(
   creativity = creativity |>
@@ -48,7 +86,7 @@ d_contr_avg <- d_contr |>
     mean_diff = mean(scorediff),
     s = sd(scorediff),
     se = s / sqrt(n()),
-    ci = qt(.025, 60 - 4, lower.tail = FALSE)
+    ci = qt(.025, 60 - 4, lower.tail = FALSE) * se
   ) |>
   ungroup()
 
