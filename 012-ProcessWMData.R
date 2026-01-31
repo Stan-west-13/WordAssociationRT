e6bc5cc3f25849759f1705a8fc40c5065686fc30
mgarce3 <mgarce3@lsu.edu>
2026-01-31 17:11:20 -0600

    made all responses lowercase and made all responses lowercase; filtered out blank responses from metadata df (can easily be changed back if needed) The updated dataWMRT.zip folder is too big to push to the github. So, for now, this script writes up an excel file of the metadata which is able to be commited to the github

--- 012-ProcessWMData.R
+++ 012-ProcessWMData.R
@@ -1,12 +1,9 @@
 library(tidyverse)
 library(readxl)
+library(writexl)
 
 # Unzip data folder
 unzip("data_WMRT.zip")
-
-tta001 <- read_xlsx('data_WMRT/TTA_001/TTA_001_LOAD_2025-09-25.xlsx')
-tta001 <- tta001 %>% 
-  mutate(condition_b = tta001[36,2], .after = condition)
 
 # Makes a list of all the files we want in our metadata df
 files <- list.files("data_WMRT", pattern = '.xlsx', recursive = T,full.names = TRUE)
@@ -14,6 +11,7 @@ files <- str_subset(files, 'PRAC', negate = T)
 
 # Takes each file from files, reads it in, creates between subjects condition column, and cleans up df a bit
 metadata <- map_dfr(files, function(x){
+  
   d <- read_xlsx(x, col_types ="text" ,trim_ws = TRUE)
   d <- d %>% 
     mutate(condition_b = d[[36,2]], .after = pp)
@@ -22,18 +20,19 @@ metadata <- map_dfr(files, function(x){
     select(participant = pp, condition_b, condition_w = condition, trial_type = TT, cue, response = input_textbox.text_raw, square_resp = yesno_resp.keys_raw,
            cue_rt = key_space.rt_mean, resp_rt = input_key.rt_mean, square_rt = yesno_resp.rt_mean) %>%
     mutate(response = gsub(response, pattern = "'", replacement = ""),
-          response = trimws(gsub(response, pattern = "\\\\n", replacement = "")),
+          response = tolower(trimws(gsub(response, pattern = "\\\\n", replacement = ""))),
           square_resp = gsub(square_resp, pattern = "'", replacement = ""))
   d <- d %>% 
     mutate(accuracy = ifelse((trial_type == "match" & square_resp == "j") | (trial_type == 'mismatch' & square_resp == 'f'), 1, 0),
            .after = response) %>% 
     mutate(across(c(participant:trial_type, accuracy, square_resp), as.factor)) %>% 
-    mutate(across(c(cue_rt:square_rt), as.numeric))
+    mutate(across(c(cue_rt:square_rt), as.numeric)) %>% 
+    filter(response != "")
   
 }) #map_dfr reads in the tables then it stacks them by rows
 
-
-
+# Export metadata df as an Excel sheet so I can put it on the github (the dataWMRT zip size gets too big to upload the entire folder)
+write_xlsx(x = metadata, path = "metadata_WMRT.xlsx")
 
 
 
