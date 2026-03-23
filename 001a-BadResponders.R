0fcf06ecb390b334b788de060cd8238a057a718a
Stanley H West <swest19@lsu.edu>
2026-03-23 10:10:16 -0500

    plot

--- 001a-BadResponders.R
+++ 001a-BadResponders.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(ggplot2)
 library(readr)
 source("R/Load_Helpers.R") 
 
@@ -10,7 +11,17 @@ cue_no_response_density <- d %>%
   summarize(resp_d = sum(response == ""|is.na(response))/n()) %>%
   mutate(is.disp = ifelse(resp_d >= (1/3), TRUE, FALSE))
 
+## Plot proportions
+ggplot(cue_no_response_density %>%
+         filter(resp_d > 0), aes(x = resp_d, y = reorder(cue, desc(-resp_d)), fill = condition))+
+  geom_bar(stat="identity")+
+  facet_grid(~condition)+
+  geom_vline(aes(xintercept =  1/3),linetype = "dashed")+
+  labs(x = "proportion of no-responses by condition", y = "cue")
 
+
+## Which participants are not responding to cues outside of the generally
+## "hard" cues. 
 response_d <- d %>% 
   group_by(condition) %>%
   mutate(low.response = ifelse(cue %in% cue_no_response_density$cue[cue_no_response_density$is.disp == TRUE], 
@@ -19,3 +30,4 @@ response_d <- d %>%
   group_by(participant) %>%
   mutate(prop_noresp = sum(((response == ""|is.na(response)) & low.response == FALSE))/n())
   
+
